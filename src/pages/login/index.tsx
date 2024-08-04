1bd7566d99f025918eb5e0c7c8e8bc099b8d27ca
jisooooooooooo <dlawltn0708@naver.com>
2024-08-04 16:03:58 +0900

    Design: 폰트 수정

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       <p className="text-white font-inter text-[14px] pl-[37px] underline mt-[27px]">
         니어니어에 요리사로 참여하고 싶으신가요?
       </p>
-      <button className="flex justify-center items-center whitespace-nowrap self-end py-[10px] px-[24px] bg-[#EEF3E2] rounded-full text-[#638404] font-robo text-[14px] font-medium leading-[20px] mt-[127px] mr-[12px]">
+      <button className="flex justify-center items-center whitespace-nowrap self-end py-[10px] px-[24px] bg-[#EEF3E2] rounded-full text-[#638404] font-pretendard text-[14px] font-medium leading-[20px] mt-[127px] mr-[12px]">
         니어니어 알아보기
       </button>
       <LoginModal isLoginRequired={false} />
