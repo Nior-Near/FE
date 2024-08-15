f426e941dee2554fb8e2cb63a0371028f7f1a2ac
jisooooooooooo <dlawltn0708@naver.com>
2024-08-15 16:24:59 +0900

    Feat: 요리사 페이지 링크 추가

--- src/components/ChefCard.tsx
+++ src/components/ChefCard.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import Link from "next/link";
 import Food from "../assets/food.jpg";
 
 export interface CardProps {
+  storeId: number;
   name: string;
   tags: string[];
   description: string;
@@ -11,6 +13,7 @@ export interface CardProps {
 }
 
 const ChefCard: React.FC<CardProps> = ({
+  storeId,
   name,
   tags,
   description,
@@ -19,43 +22,45 @@ const ChefCard: React.FC<CardProps> = ({
   imageUrl,
 }) => {
   return (
-    <div className="bg-white rounded-[12px] w-[322px] overflow-hidden shadow-md">
-      <div className="relative">
-        <img
-          src={Food.src}
-          alt="이미지 들어가요"
-          className="w-[322px] h-[179px] object-cover"
-        />
-      </div>
-      <div className="p-[16px]">
-        <div className="flex items-center">
+    <Link href={`/stores/${storeId}`} passHref>
+      <div className="bg-white rounded-[12px] w-[322px] overflow-hidden shadow-md">
+        <div className="relative">
           <img
-            src={imageUrl}
-            alt="요리사사진"
-            className="h-[28px] w-[28px] rounded-full mr-[8px]"
+            src={Food.src}
+            alt="이미지 들어가요"
+            className="w-[322px] h-[179px] object-cover"
           />
-          <div className="font-pretendard text-[14px] leading-[22px]">
-            {name} 요리사
+        </div>
+        <div className="p-[16px]">
+          <div className="flex items-center">
+            <img
+              src={imageUrl}
+              alt="요리사사진"
+              className="h-[28px] w-[28px] rounded-full mr-[8px]"
+            />
+            <div className="font-pretendard text-[14px] leading-[22px]">
+              {name} 요리사
+            </div>
+          </div>
+          <div className="font-pretendard h-[39px] flex items-center text-[16px] leading-[25px] whitespace-nowrap ">
+            {description}
+          </div>
+          <div className="flex flex-row items-center gap-[8px] mb-[12px] flex-wrap">
+            {tags.map((tag, index) => (
+              <div
+                key={index}
+                className="px-[4px] flex items-center justify-center text-[#638404] whitespace-nowrap bg-[#EEF3E2] rounded-[2px] font-pretendard text-[12px] leading-[19px]"
+              >
+                {tag}
+              </div>
+            ))}
+          </div>
+          <div className="text-[#707A87] font-pretendard text-[12px] leading-[19px]">
+            온도 {temperature} | 후기 {reviews}개
           </div>
         </div>
-        <div className="font-pretendard h-[39px] flex items-center text-[16px] leading-[25px] whitespace-nowrap ">
-          {description}
-        </div>
-        <div className="flex flex-row items-center gap-[8px] mb-[12px] flex-wrap">
-          {tags.map((tag, index) => (
-            <div
-              key={index}
-              className="px-[4px] flex items-center justify-center text-[#638404] whitespace-nowrap bg-[#EEF3E2] rounded-[2px] font-pretendard text-[12px] leading-[19px]"
-            >
-              {tag}
-            </div>
-          ))}
-        </div>
-        <div className="text-[#707A87] font-pretendard text-[12px] leading-[19px]">
-          온도 {temperature} | 후기 {reviews}개
-        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
