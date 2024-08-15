983db48fb7d7160a295b16f645ec3a434ddcb0f2
YH <imrubi5555@gmail.com>
2024-08-16 01:07:52 +0900

    마이페이지 긴급 API 연동

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -3,8 +3,17 @@ import NavigateNext from "@/src/assets/navigate_next.svg";
 import Envelope from "@/src/assets/envelope.svg";
 import { useState } from "react";
 import LoginModal from "@/src/components/LoginModal";
+import { axios } from "@/src/lib/axios";
+import { GetServerSidePropsContext } from "next";
 
-export default function My() {
+interface Data {
+  memberId: number;
+  nickname: string;
+  point: number;
+  image_url: string;
+}
+
+export default function My({ data }: { data?: Data }) {
   const [showLogin, setShowLogin] = useState(false);
 
   return (
@@ -23,75 +32,79 @@ export default function My() {
         <div className="top-[151px] absolute flex-col justify-start items-end gap-[29px] inline-flex">
           <div className="px-[24px] w-full self-stretch justify-center items-end gap-[40px] inline-flex">
             <div className="justify-start items-end gap-[13px] flex">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="88"
-                height="88"
-                viewBox="0 0 104 104"
-                fill="none"
-              >
-                <g filter="url(#filter0_d_194_3587)">
-                  <rect x="8" y="8" width="88" height="88" rx="44" fill="#DBE8B6" />
-                  <rect
-                    x="6.5"
-                    y="6.5"
-                    width="91"
-                    height="91"
-                    rx="45.5"
-                    stroke="white"
-                    strokeWidth="3"
-                  />
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M65.2005 43.2C65.2005 50.4902 59.2907 56.4 52.0005 56.4C44.7104 56.4 38.8005 50.4902 38.8005 43.2C38.8005 35.9098 44.7104 30 52.0005 30C59.2907 30 65.2005 35.9098 65.2005 43.2ZM60.8005 43.2C60.8005 48.0601 56.8606 52 52.0005 52C47.1404 52 43.2005 48.0601 43.2005 43.2C43.2005 38.3399 47.1404 34.4 52.0005 34.4C56.8606 34.4 60.8005 38.3399 60.8005 43.2Z"
-                    fill="#638404"
-                  />
-                  <path
-                    d="M52.0005 63C37.7569 63 25.6209 71.4225 20.998 83.2225C22.1242 84.3408 23.3106 85.3985 24.5518 86.3905C27.9942 75.5569 38.7933 67.4 52.0005 67.4C65.2078 67.4 76.0069 75.5569 79.4493 86.3906C80.6905 85.3986 81.8769 84.3408 83.003 83.2225C78.3802 71.4225 66.2442 63 52.0005 63Z"
-                    fill="#638404"
-                  />
-                </g>
-                <defs>
-                  <filter
-                    id="filter0_d_194_3587"
-                    x="0"
-                    y="0"
-                    width="104"
-                    height="104"
-                    filterUnits="userSpaceOnUse"
-                    colorInterpolationFilters="sRGB"
-                  >
-                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
-                    <feColorMatrix
-                      in="SourceAlpha"
-                      type="matrix"
-                      values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                      result="hardAlpha"
+              {data ? (
+                <img src={data?.image_url} width={88} height={88} />
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="88"
+                  height="88"
+                  viewBox="0 0 104 104"
+                  fill="none"
+                >
+                  <g filter="url(#filter0_d_194_3587)">
+                    <rect x="8" y="8" width="88" height="88" rx="44" fill="#DBE8B6" />
+                    <rect
+                      x="6.5"
+                      y="6.5"
+                      width="91"
+                      height="91"
+                      rx="45.5"
+                      stroke="white"
+                      strokeWidth="3"
                     />
-                    <feOffset />
-                    <feGaussianBlur stdDeviation="2.5" />
-                    <feComposite in2="hardAlpha" operator="out" />
-                    <feColorMatrix
-                      type="matrix"
-                      values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M65.2005 43.2C65.2005 50.4902 59.2907 56.4 52.0005 56.4C44.7104 56.4 38.8005 50.4902 38.8005 43.2C38.8005 35.9098 44.7104 30 52.0005 30C59.2907 30 65.2005 35.9098 65.2005 43.2ZM60.8005 43.2C60.8005 48.0601 56.8606 52 52.0005 52C47.1404 52 43.2005 48.0601 43.2005 43.2C43.2005 38.3399 47.1404 34.4 52.0005 34.4C56.8606 34.4 60.8005 38.3399 60.8005 43.2Z"
+                      fill="#638404"
                     />
-                    <feBlend
-                      mode="normal"
-                      in2="BackgroundImageFix"
-                      result="effect1_dropShadow_194_3587"
+                    <path
+                      d="M52.0005 63C37.7569 63 25.6209 71.4225 20.998 83.2225C22.1242 84.3408 23.3106 85.3985 24.5518 86.3905C27.9942 75.5569 38.7933 67.4 52.0005 67.4C65.2078 67.4 76.0069 75.5569 79.4493 86.3906C80.6905 85.3986 81.8769 84.3408 83.003 83.2225C78.3802 71.4225 66.2442 63 52.0005 63Z"
+                      fill="#638404"
                     />
-                    <feBlend
-                      mode="normal"
-                      in="SourceGraphic"
-                      in2="effect1_dropShadow_194_3587"
-                      result="shape"
-                    />
-                  </filter>
-                </defs>
-              </svg>
+                  </g>
+                  <defs>
+                    <filter
+                      id="filter0_d_194_3587"
+                      x="0"
+                      y="0"
+                      width="104"
+                      height="104"
+                      filterUnits="userSpaceOnUse"
+                      colorInterpolationFilters="sRGB"
+                    >
+                      <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                      <feColorMatrix
+                        in="SourceAlpha"
+                        type="matrix"
+                        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                        result="hardAlpha"
+                      />
+                      <feOffset />
+                      <feGaussianBlur stdDeviation="2.5" />
+                      <feComposite in2="hardAlpha" operator="out" />
+                      <feColorMatrix
+                        type="matrix"
+                        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+                      />
+                      <feBlend
+                        mode="normal"
+                        in2="BackgroundImageFix"
+                        result="effect1_dropShadow_194_3587"
+                      />
+                      <feBlend
+                        mode="normal"
+                        in="SourceGraphic"
+                        in2="effect1_dropShadow_194_3587"
+                        result="shape"
+                      />
+                    </filter>
+                  </defs>
+                </svg>
+              )}
               <div className="text-[#707a87] text-[16px] font-[400] font-pretendard leading-[25.6px]">
-                로그인해주세요.
+                {data ? data?.nickname : "로그인해주세요."}
               </div>
             </div>
             <div className="w-[85px] py-[5px] flex-col justify-start items-start gap-1 inline-flex">
@@ -122,7 +135,7 @@ export default function My() {
                 </div>
               </div>
               <div className="text-white text-[28px] font-[600] font-pretendard leading-none">
-                0p
+                {data ? data?.point : "0"}p
               </div>
             </div>
 
@@ -252,4 +265,14 @@ export default function My() {
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  try {
+    const response = await axios.get(`/users`);
+
+    return { props: { data: response.data?.result } };
+  } catch (e) {
+    return { props: {} };
+  }
 }
