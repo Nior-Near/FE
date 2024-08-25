79945c91c9797fca41b317a30d0b58190bfa3e0a
YH <imrubi5555@gmail.com>
2024-08-25 16:29:52 +0900

    feat: api 도메인 변경

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
 export const axios = Axios.create({
-  baseURL: "https://api.niornear.store/",
+  baseURL: "https://test.niornear.store/",
   withCredentials: true,
 });
