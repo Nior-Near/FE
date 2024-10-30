64fe4b058d5bc694381fdcba4910a468eb4549d5
jisooooooooooo <dlawltn0708@naver.com>
2024-10-31 00:17:33 +0900

    Fix: 로그인 경로 수정

--- src/pages/auth/oauth-response.tsx
+++ src/pages/auth/oauth-response.tsx
@@ -1,31 +1,31 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-
 export default function OAuthResponse() {
   const router = useRouter();
-
   useEffect(() => {
     const storeAccessToken = async () => {
-      const { token, redirect } = router.query;
+      const { token } = router.query;
+
       if (token) {
         localStorage.setItem("accessToken", token as string);
+        console.log("AccessToken 저장:", token);
 
-        const baseRedirectUrl = process.env.NEXT_PUBLIC_BASE_URL;
+        const baseRedirectUrl =
+          window.location.hostname === "localhost"
+            ? "http://localhost:3000"
+            : "https://www.niornear.store";
 
-        const redirectPath = redirect ? (redirect as string) : "/home";
+        const redirectPath = (router.query.redirect as string) || "/home";
         router.push(`${baseRedirectUrl}${redirectPath}`);
-
       } else {
         console.error("토큰이 없습니다.");
         router.push("/login");
       }
     };
-
-    if (router.isReady && router.query.token) {
+    if (router.isReady) {
       storeAccessToken();
     }
-  }, [router.isReady, router.query.token]);
-
+  }, [router.isReady, router.query]);
   return (
     <div className="flex justify-center items-center h-screen">
       <p>로그인 중...</p>
