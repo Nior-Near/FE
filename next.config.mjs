d7e7ec97d1425e4c0b274151d06214d00e237737
YH <imrubi5555@gmail.com>
2024-10-31 17:43:06 +0900

    Chore: 이미지 도메인 추가

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "ssl.pstatic.net",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
